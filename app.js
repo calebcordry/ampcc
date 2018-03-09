3bfe822cc137931b53144e5eb109bf6f5f694859
calebcordry <ccordry@google.com>
2018-03-09 15:21:44 -0800

    linter clean up

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require('express');
-const path = require('path');
 
-const {getRandomInt} = require('./helper');
+const { enableCors, getRandomInt } = require('./helper');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
 app.get('/story', (req, res) => {
-  res.sendFile('/public/story.html', { root : __dirname});
+  res.sendFile('/public/story.html', { root: __dirname });
 });
 
 app.get('/api/track', (req, res) => {
@@ -20,21 +17,9 @@ app.get('/api/track', (req, res) => {
 
 app.get('/api/ad', (req, res) => {
   const choice = getRandomInt(6) + 1;
-  res.sendFile(`/ad-server/response${choice}.json`, { root : __dirname});
   const origin = req.query.__amp_source_origin || req.hostname;
   enableCors(req, res, origin);
-})
+  res.sendFile(`/ad-server/response${choice}.json`, { root: __dirname });
+});
 
 app.listen(PORT, () => console.log('Server listening on port 3000!'));
-
-function enableCors(req, res, origin, opt_exposeHeaders) {
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Origin', origin);
-  res.setHeader('Access-Control-Expose-Headers',
-      ['AMP-Access-Control-Allow-Source-Origin']
-          .concat(opt_exposeHeaders || []).join(', '));
-  if (req.query.__amp_source_origin) {
-    res.setHeader('AMP-Access-Control-Allow-Source-Origin',
-        req.query.__amp_source_origin);
-  }
-}
--- helper/index.js
+++ helper/index.js
@@ -1,4 +1,23 @@
 const getRandomInt = max =>
   Math.floor(Math.random() * Math.floor(max));
 
-module.exports = {getRandomInt};
+function enableCors(req, res, origin, optExposeHeaders) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', origin);
+  res.setHeader(
+    'Access-Control-Expose-Headers',
+    ['AMP-Access-Control-Allow-Source-Origin']
+      .concat(optExposeHeaders || []).join(', '),
+  );
+  if (req.query.__amp_source_origin) {
+    res.setHeader(
+      'AMP-Access-Control-Allow-Source-Origin',
+      req.query.__amp_source_origin,
+    );
+  }
+}
+
+module.exports = {
+  getRandomInt,
+  enableCors,
+};
