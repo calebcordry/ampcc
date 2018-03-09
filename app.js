585143c57226e3566e0568a3c3569ff9f11b89a1
calebcordry <ccordry@google.com>
2018-03-09 15:37:08 -0800

    remove bad vid

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { enableCors, getRandomInt } = require('./helper');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/story', (req, res) => {
   res.sendFile('/public/story.html', { root: __dirname });
@@ -16,10 +16,10 @@ app.get('/api/track', (req, res) => {
 });
 
 app.get('/api/ad', (req, res) => {
-  const choice = getRandomInt(6) + 1;
+  const choice = getRandomInt(5) + 1;
   const origin = req.query.__amp_source_origin || req.hostname;
   enableCors(req, res, origin);
   res.sendFile(`/ad-server/response${choice}.json`, { root: __dirname });
 });
 
-app.listen(PORT, () => console.log('Server listening on port 3000!'));
+app.listen(PORT, () => console.log('Server listening on port 3000!')); // eslint-disable-line no-console
